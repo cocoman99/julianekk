cd009c2f7cb8239698566879ba48f0096da0eb47
cocoman99 <cocoman99@o2.pl>
2024-12-06 12:12:10 +0100

    Graphic visualization

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,27 @@ fn main() {
     let timetime = time::Duration::from_millis(125);
     let mut counter = 1;
     let mut iter = 0;
+
+    /*
+     2D |   | COL                                     COL
+    ____|___|__1__2__3__4__5__6__7__8__9__10__11__12__13___
+    ROW | 1 |  J  J
+        | 2 |  U  U 
+        | 3 |  L  L 
+        | 4 |  I  I
+        | 5 |  A  A
+        | 6 |  N  N
+        | 7 |  E  E
+        | 8 |  K  K
+        | 9 |  K  K
+        |10 |
+        |11 |  M  M
+        |12 |  A  A
+        |13 |  T  T
+        |14 |  R  R
+        |15 |  I  I
+    ROW |16 |  X  X
+    */
     
     let julianekk =
     [" J", " U", " L", " I", " A", " N", " E", " K", " K", " ", 
